f8489d897e05f2fd82018920eff2d49d1105bc84
Gerar <gerbas.cc@gmail.com>
2022-11-10 15:06:22 -0300

    edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          EN REACT/ <code>Mi PRIMERA EDICION</code> VAMOS POR MAS.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          INGRESAR
         </a>
       </header>
     </div>
